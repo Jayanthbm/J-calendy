3ec0f9f62aca39c8f3f7c20f84878c83353adefb
jayanth bharadwaj <jayanthbharadwaj@jayanths-MacBook-Air.local>
2020-04-17 00:08:59 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const routes = require('./router/router');
+const routes = require('./src/router/router');
 
+var port = process.env.PORT || 4000;
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
 
-app.listen(4000, () => {
-    console.log("Server running at port 4000")
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`)
 })
